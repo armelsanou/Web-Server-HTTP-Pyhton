7a9b2ba77b132f30df5f61355c181de2168078de
armelsanou <32012849+armelsanou@users.noreply.github.com>
2022-04-13 12:59:02 +0200

    Create test.py
    ok

---
+++ test.py
@@ -0,0 +1,7 @@
+print("Créer une application serveur (serveurhttp.py) permettant à un navigateur de consulter des «pages web». Il faudra
+pour cela consulter un peu de documentation sur le protocole HTTP et le format des requêtes/messages (il n’est pas
+nécessaire de tenter d’implanter tout le protocole HTTP).
+Note : si l’url requête «client» est http://adresse:port/truc/bidule.html, que le serveur s’exécute (par exemple)
+dans le répertoire /home/serveur alors le fichier qui sera transmis sera /home/serveur/truc/bidule.html.
+Note : les seuls modules Python devant être utilisés sont les modules de base comme (éventuellement) re, threading,
+socket, sys, urllib. Il est interdit d’utiliser des modules comme http ou du genre...")
